a7f8031bbf48dfc6c87027baa5edfc28e15026e3
maria majan <mavi.majan@gmail.com>
2022-04-15 21:46:09 -0300

    DOM

--- index.js
+++ index.js
@@ -1,82 +1,74 @@
-let usuario = prompt("Ingresar Usuario");
+// PRODUCTOS A LA VENTA
 
-while (usuario =""){
-    alert("intentalo de nuevo");
-    usuario = prompt("Ingresa Usuario");
+const array=[{id:1, conjunto: "Conjunto Berlín"},
+            {id: 2, conjunto: "Conjunto Amsterdam"},
+            {id: 3, conjunto: "Conjunto Roma"}];
+console.log(array [1]);
+array.push({id:4, conjunto:"Conjunto Venecia"});
+console.log(array.length);
+console.log(array);
+
+for(const conjunto of array){
+    console.log(conjunto);
+    console.log(conjunto.array);
 }
 
-for (let i= 1; i<4; i ++){
-    let usuario = prompt("Ingresar usuario");
-    if (usuario =""){
-        alert("Espera unos minutos y vuelve a intentar");
+
+// PRODUCTOS VENDIDOS
+class Producto{
+    constructor(nombre, precio){
+        this.nombre= nombre.toUpperCase();
+        this.precio= parseInt(precio);
+        this.vendido= false;
+    }
+    sumaIva(){
+        this.precio= this.precio*1.21;
+    }
+    vender(){
+        this.vendido= true;
     }
 }
-if(usuario !=""){
-    let contraseña = prompt("Ingresar contraseña");
-    for (let i= 1; i<4; i++){
-        let contraseña = prompt("Ingresar contraseña");
-        alert("Escribenos para recuperar tu contraseña");
+
+const producto1= new Producto ("conjunto Berlin","1400");
+const producto2= new Producto ("conjunto Venecia","1500");
+const producto3= new Producto ("conjunto Roma", "1800");
+const producto4= new Producto ("conjunto Amsterdam", "1600");
+producto1.sumaIva();
+producto2.sumaIva();
+producto3.sumaIva();
+producto4.sumaIva();
+
+console.log(producto1.vendido);
+producto2.vender();
+console.log (producto2. vendido);
+producto3.vender();
+console.log (producto3. vendido);
+producto4.vender();
+console.log (producto4. vendido);
+
+// DOM
+
+let ropaInterior =document.createElement("h3");
+ropaInterior.innerHTML= "<h3> Ropa Interior </h3>";
+document.body.append(ropaInterior);
+
+let iniciarSesion = document.getElementById("iniciarSesion");
+iniciarSesion.addEventListener("click", respuesta)
+function respuesta(){
+    let usuario= prompt ("Ingrese usuario");
+    let contrasenia= prompt("Ingrese contraseña");
+    if(!(usuario === '' && contrasenia === '')){
+                alert("Bienvenida a nuestro sitio");
+    }else{
+        alert(`usuario o contraseña incorrectos, intentalo nuevamente`)
+            usuario= prompt("Ingrese usuario");
+            contrasenia= parseInt(prompt("Ingrese contraseña"));
     }
 }
 
-let contraseña = prompt("Ingresar Contraseña");
-let contraseña = prompt ("Ingresá contraseña");
-const salida = usuario + " " + "Puedes comenzar a comprar";
-
-
-if ((usuario !="") && (contraseña !="")){
-    alert(salida);
-}else if (usuario !=""){
-    alert("Intentalo nuevamente");
-    usuario = prompt("Ingresar usuario");
-}
-if (usuario !=""){
-    console.log("Ingresa Contraseña");
-    console.log(contraseña = prompt("Ingresa contraseña"));
-}else if (usuario =""){
-    console.log(alert("Intentalo Nuevamente"));
-    console.log(usuario = prompt("Ingresar Usuario"));
-}else if((usuario !="") && (contraseña ="")){
-    console.log(alert("Intentalo Nuevamente"));
-    console.log(contraseña = prompt("Ingresá Contraseña"));
-}else{
-    ((usuario !="") && (contraseña !=""))
-    console.log(alert(salida));
+let id = document.getElementById("shop");
+id.addEventListener("click", respuesta1)
+function respuesta1(){
+    alert("Puedes comenzar a comprar");
 }
 
-if ((usuario !="") && (contraseña !="")){
-    alert(salida);
-}else if (usuario =""){
-    alert("Intentalo Nuevamente");
-    usuario = prompt("Ingresar Usuario");
-}else if (usuario !=""){
-    alert("Ingresa Contraseña");
-    contraseña = prompt("Ingresa contraseña");
-}else{
-    ((usuario !="") && (contraseña =""))
-    alert("Intentalo Nuevamente");
-    contraseña = prompt("Ingresá Contraseña");
-}
-
-if ((usuario !="") && (contraseña !="")){
-    alert(salida);
-}else{
-    ((usuario ="") || (contraseña =""))
-    alert("Intentalo nuevamente");
-    usuario = prompt("Ingresa Usuario");
-    contraseña = prompt("Ingresa Contraseña");
-}
-
-let usuario = prompt("Ingrese usuario");
-let contraseña = prompt("Ingrese contraseña");
-let salida = usuario + " " + "Puedes comenzar a comprar";
-
-if (usuario !=""){
-    let contraseña = prompt("Ingrese contraseña");
-    if (contraseña !=""){
-        alert(salida);
-    }
-}else{
-    alert("Error: ingresar usuario valido");
-    usuario = prompt("Ingrese usuario");
-}
